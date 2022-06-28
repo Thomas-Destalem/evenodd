6d40b45dc97d33004e4911d8492196905e1218e7
Thomas-Destalem <107938724+Thomas-Destalem@users.noreply.github.com>
2022-06-28 19:38:48 +0300

    Add files via upload

---
+++ even and odd/even and odd/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("enter the number");
+int  x;
+string y = Console.ReadLine();
+string n = Console.ReadLine();
+
+x=Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i <= 3; i++)
+{
+
+
+    if (x%2==0)
+    {
+        Console.WriteLine("the no is even");
+    }
+    else
+    {
+        Console.WriteLine("the no is odd");
+    }
+    
+}
+
+
+
+
+
+
